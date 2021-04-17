ae4a01756b12c7954cb12007d4c3c4be1e76b4d7
Philip Jones <pmj44@drexel.edu>
2021-04-17 13:41:04 -0400

    added test data function

---
+++ data/test_data.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+import numpy as np
+import json
+
+def getTestData():
+    print(os.getcwd())
+    imageFiles = os.listdir("./data/images")
+    jsonData = json.load(open("data/test_data.json", "r"))
+
+    imageArray = []
+    jsonArray = []
+
+    for f in imageFiles:
+        img = Image.open("./data/images/" + f)
+        imageArray.append(np.array(img))
+        jsonArray.append(jsonData)
+
+    return np.array(imageArray), np.array(jsonArray)
